a25e116d5a128b3040d8c32bfa001ad38df53c8d
gauravnainwal229 <gauravnainwal229@gmail.com>
2025-07-20 21:37:16 +0530

    fix: resolve featured image selection issue by correcting file input handling in PostForm

--- Mega_blog/src/components/post-form/PostForm.jsx
+++ Mega_blog/src/components/post-form/PostForm.jsx
@@ -164,10 +164,11 @@ function PostForm({ post }) {
           accept="image/png, image/jpg, image/jpeg, image/gif"
           {...register("image", {
             required: !post ? "Featured image is required" : false,
+            //  This handles the image properly
+            onChange: (e) => {
+              return e.target.files;
+            },
           })}
-          onChange={(e) => {
-            setValue("image", e.target.files); // ensure image gets into form state
-          }}
         />
 
         {post?.featuredImage && (
